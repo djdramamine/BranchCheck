84b9669a8bd940097459b4a5567fc04670ee4555
Daniel J Dusharm <dushar88@students.rowan.edu>
2020-12-23 00:06:52 +0000

    fuck around on wednesday

--- FiveIncrementer.cpp
+++ FiveIncrementer.cpp
@@ -1,9 +1,9 @@
 #include "FiveIncrementer.h"
 
 int FiveIncrementer::increment(int x) {
-    return 5+x; //No it isn't
+    return 4+x; //No it isn't it's 4
 }
 
 int FiveIncrementer::decrement(int x) {
-    return x-5; //No it isnt
+    return x-4; //No it isnt it's 4
 }
